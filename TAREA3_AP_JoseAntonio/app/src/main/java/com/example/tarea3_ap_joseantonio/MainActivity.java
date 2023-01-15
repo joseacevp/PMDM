38e2446b03cb6e868db02cdb12bfb393c8f39a3f
Jose Ace <joseacevp@gmail.com>
2023-01-15 19:53:38 +0100

    tarea 3 PMDM
    ICONOS 
    Inicio de implementación del cardView en Fragment bicicletas.

---
+++ TAREA3_AP_JoseAntonio/app/src/main/java/com/example/tarea3_ap_joseantonio/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.tarea3_ap_joseantonio;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity {
+    HomeFragment homeFragment = new HomeFragment();
+    CalenFragment calenFragment = new CalenFragment();
+    BicleFragment bicleFragment = new BicleFragment();
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+         loadFragment(homeFragment);
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch(item.getItemId()) {
+                case R.id.homeFragment:
+                    loadFragment(homeFragment);
+                    return true;
+                case R.id.biciFragment:
+                    loadFragment(bicleFragment);
+                    return true;
+                case R.id.calendarioFragment:
+                    loadFragment(calenFragment);
+                    return true;
+              }
+
+            return false;
+        }
+    };
+
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_cotainer,fragment);
+        transaction.commit();
+    }
+
+
+
+
+}
