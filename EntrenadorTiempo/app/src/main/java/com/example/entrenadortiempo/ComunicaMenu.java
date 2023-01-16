825a94746fffc39e19a76bb354765d53f5cb1eb4
Jose Ace <joseacevp@gmail.com>
2023-01-16 20:20:47 +0100

    Solo tarea 3 pmdp
    Limpieza de repositorio PMDM
    Queda solo el proyecto de La Tarea 3 
    Tarea 3 CardView con menú Navegación inferior.
    El menú de navegación esta implementado y funciona bien 
    Seguir con el desarrollo del CardView.

--- EntrenadorTiempo/app/src/main/java/com/example/entrenadortiempo/ComunicaMenu.java
+++
@@ -1,8 +0,0 @@
-package com.example.entrenadortiempo;
-
-import android.widget.Button;
-
-public interface ComunicaMenu {
-    public void menu(int tiempoPre, int tiempTrab, int tiempoDesc, int numCicl,boolean iniciado);
-
-}
