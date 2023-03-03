d3d1a5cb27d9eec618f217eb6dcc8e39c116c40e
Jose Ace <joseacevp@gmail.com>
2023-03-03 11:16:09 +0100

    menú iniciado

--- Tarea3Segundo/app/src/main/java/com/example/tarea3segundo/MainActivity.java
+++ Tarea3Segundo/app/src/main/java/com/example/tarea3segundo/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.tarea3segundo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import com.example.tarea3segundo.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.MenuItem;
+
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    switch(item.getItemId()) {
+                        case R.id.homeFragment:
+                            //loadFragment(homeFragment);
+                            return true;
+                        case R.id.biciFragment:
+                           // loadFragment(bicleFragment);
+                            return true;
+                        case R.id.calendarioFragment:
+                            //loadFragment(calenFragment);
+                            return true;
+                    }
+
+                    return false;
+
+                }
+            };
+    @Override
+    public void onClick(View v) {
+
     }
 }
