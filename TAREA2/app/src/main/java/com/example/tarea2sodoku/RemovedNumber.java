6cbe07be12aead66d49bb2536de2ba5679d9dbd6
Jose Ace <joseacevp@gmail.com>
2023-10-30 20:40:41 +0100

    Tarea 2 Sudoku INICIO
    Esqueleto operativo
    Seguir con los apartados de la Tarea

---
+++ TAREA2/app/src/main/java/com/example/tarea2sodoku/RemovedNumber.java
@@ -0,0 +1,13 @@
+package com.example.tarea2sodoku;
+
+public class RemovedNumber {
+    public int row;
+    public int col;
+    public int value;
+
+    public RemovedNumber(int row, int col, int value) {
+        this.row = row;
+        this.col = col;
+        this.value = value;
+    }
+}
