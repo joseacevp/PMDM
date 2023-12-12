55b175aa907c1a721ad75094f6114c08d6d0dee3
Jose Ace <joseacevp@gmail.com>
2023-12-12 06:42:44 +0100

    Tarea 3
    Pantalla configuración
    Spinners Héroe y Dificultad funcionales

---
+++ TAREAS/TAREA3/JuegoMultiplicar/app/src/main/java/com/example/juegomultiplicar/ConfiguracionFragment.java
@@ -0,0 +1,160 @@
+package com.example.juegomultiplicar;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ConfiguracionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ConfiguracionFragment extends Fragment {
+    View view;
+    String[] ciudades = {"Batman",
+            "Hulk",
+            "Airon Man",
+            "Capitan America"};
+
+    String[] descripcion = {"Matches Malone\u200B El caballero de la noche El caballero oscuro Zurdo Knox\u200B El señor de la noche\u200B",
+            "Hulk, El Increíble Hulk,\u200B El Justiciero, El Hombre Increíble",
+            "Tony Stark, Iron Man (El Hombre de Hierro)",
+            "Nómada, El Capitán, Cap, Steve Rogers, Steve"
+            };
+
+    int imagenes[] = {R.drawable.batmancuatro,
+            R.drawable.huld_cuatro,
+            R.drawable.iron_cuatro,
+            R.drawable.capi_cuatro
+    };
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ConfiguracionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ConfiguracionFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ConfiguracionFragment newInstance(String param1, String param2) {
+        ConfiguracionFragment fragment = new ConfiguracionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_configuracion, container, false);
+
+        Spinner selectorAbatar = view.findViewById(R.id.spinner_avatar);
+        AdaptadorPersonalizado a = new AdaptadorPersonalizado(getContext(),
+                R.layout.linea_personajes, ciudades);
+        selectorAbatar.setAdapter(a);
+
+        selectorAbatar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //codigo para gestionar lo que ocurre cuando seleccionas una opcion del Spinner
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Spinner selectorDificultad = view.findViewById(R.id.spinner_dificultad);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(getContext(),R.array.array_dificultad,
+                android.R.layout.simple_spinner_item);
+        selectorDificultad.setAdapter(adapter);
+
+        return view;
+
+    }
+
+
+    public class AdaptadorPersonalizado extends ArrayAdapter {
+
+
+        public AdaptadorPersonalizado(@NonNull Context context,
+                                      int textViewResourceId,
+                                      @NonNull Object[] objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getDropDownView(int position,
+                                    @Nullable View convertView,
+                                    @NonNull ViewGroup parent) {
+            return crearFilaPersonalizada(position, convertView, parent);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position,
+                            @Nullable View convertView,
+                            @NonNull ViewGroup parent) {
+            return crearFilaPersonalizada(position, convertView, parent);
+        }
+
+        private View crearFilaPersonalizada(int position,
+                                            View convertView,
+                                            ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View miFila = inflater.inflate(R.layout.linea_personajes,
+                    parent, false);
+
+            TextView nombre = miFila.findViewById(R.id.nombre);
+            nombre.setText(ciudades[position]);
+
+            TextView descipcion = miFila.findViewById(R.id.descripcion);
+            descipcion.setText(descripcion[position]);
+
+            ImageView imagen = miFila.findViewById(R.id.imagen_heroe);
+            imagen.setImageResource(imagenes[position]);
+            return miFila;
+        }
+    }
+    public class AdaptadorDificultad extends ArrayAdapter{
+        public AdaptadorDificultad(@NonNull Context context, int resource) {
+            super(context, resource);
+            
+        }
+
+    }
+
+}
