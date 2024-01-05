9580b1e42547ec028f719c133f9d829dd98e3857
Jose Ace <joseacevp@gmail.com>
2024-01-05 06:32:03 +0100

    Tarea 3 v2
    spinner heroe OK
    sigue con spinner dificultad

---
+++ TAREAS/TAREA3/MaestroMultiplicador/app/src/main/java/com/example/maestromultiplicador/ui/AdaptadorPersonalizado.java
@@ -0,0 +1,102 @@
+package com.example.maestromultiplicador.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.maestromultiplicador.R;
+
+public class AdaptadorPersonalizado extends ArrayAdapter{
+
+    private String[] heroes = {"Batman",
+            "Hulk",
+            "Iron Man",
+            "Capitan America"};
+
+    private String[] descripcion = {"Matches Malone\u200B El caballero de la noche El caballero oscuro Zurdo Knox\u200B El señor de la noche\u200B",
+            "Hulk, El Increíble Hulk,\u200B El Justiciero, El Hombre Increíble",
+            "Tony Stark, Iron Man (El Hombre de Hierro)",
+            "Nómada, El Capitán, Cap, Steve Rogers, Steve"
+    };
+
+    private int imagenes[] = {R.drawable.batman,
+            R.drawable.huld,
+            R.drawable.iron,
+            R.drawable.capi
+    };
+
+    public String[] getHeroes() {
+        return heroes;
+    }
+
+    public void setHeroes(String[] heroes) {
+        this.heroes = heroes;
+    }
+
+    public String[] getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String[] descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int[] getImagenes() {
+        return imagenes;
+    }
+
+    public void setImagenes(int[] imagenes) {
+        this.imagenes = imagenes;
+    }
+    //adaptador personalizado para el spinner de los heroes
+
+
+        public AdaptadorPersonalizado(@NonNull Context context,
+                                      int textViewResourceId,
+                                      @NonNull Object[] objects) {
+
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getDropDownView(int position,
+                                    @Nullable View convertView,
+                                    @NonNull ViewGroup parent) {
+            return crearFilaPersonalizada(position, convertView, parent);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position,
+                            @Nullable View convertView,
+                            @NonNull ViewGroup parent) {
+            return crearFilaPersonalizada(position, convertView, parent);
+        }
+
+        private View crearFilaPersonalizada(int position,
+                                            View convertView,
+                                            ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            View miFila = inflater.inflate(R.layout.linea_personajes,
+                    parent, false);
+
+            TextView nombre = miFila.findViewById(R.id.nombre);
+            nombre.setText(heroes[position]);
+
+            TextView descipcion = miFila.findViewById(R.id.descripcion);
+            descipcion.setText(descripcion[position]);
+
+            ImageView imagen = miFila.findViewById(R.id.imagen_heroe);
+            imagen.setImageResource(imagenes[position]);
+            return miFila;
+        }
+
+
+
+}
--- TAREAS/TAREA3/MaestroMultiplicador/app/src/main/java/com/example/maestromultiplicador/ui/ConfigurarFragment.java
+++ TAREAS/TAREA3/MaestroMultiplicador/app/src/main/java/com/example/maestromultiplicador/ui/ConfigurarFragment.java
@@ -6,17 +6,64 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
+import com.example.maestromultiplicador.R;
 import com.example.maestromultiplicador.databinding.FragmentConfigurarBinding;
 
 public class ConfigurarFragment extends Fragment {
 
     private FragmentConfigurarBinding binding;
+    //variables
+    private View view;
+    private String heroe, dificultad, fechaSeleccionada, tabla, numero_tabla_selec;
+
+    private String[] heroes = {"Batman",
+            "Hulk",
+            "Iron Man",
+            "Capitan America"};
+
+//    private String[] descripcion = {"Matches Malone\u200B El caballero de la noche El caballero oscuro Zurdo Knox\u200B El señor de la noche\u200B",
+//            "Hulk, El Increíble Hulk,\u200B El Justiciero, El Hombre Increíble",
+//            "Tony Stark, Iron Man (El Hombre de Hierro)",
+//            "Nómada, El Capitán, Cap, Steve Rogers, Steve"
+//    };
+//
+//    private int imagenes[] = {R.drawable.batman,
+//            R.drawable.huld,
+//            R.drawable.iron,
+//            R.drawable.capi
+//    };
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentConfigurarBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        //spinner selector de abatar
+        Spinner selectorAbatar = root.findViewById(R.id.spinner_avatar);
+        AdaptadorPersonalizado a = new AdaptadorPersonalizado(getContext(),
+                R.layout.linea_personajes, heroes);
+        selectorAbatar.setAdapter(a);
+
+        selectorAbatar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //codigo para gestionar lo que ocurre cuando seleccionas una opcion del Spinner
+                heroe = adapterView.getItemAtPosition(i).toString();
+//                Toast.makeText(getContext(), heroe, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        //spinner selector dificulatad
 
         return root;
     }
