2af9315a39499f8802bbb4a68df58a7b7ddbf6e8
Jose Ace <joseacevp@gmail.com>
2024-01-16 06:44:52 +0100

    Febrero 2023 Practica Examen
    Funcional
    Falta desarrollar método calcular calorías.

---
+++ EJEMPLOS_EXAMENES/FEBRERO/ExamenFebrero23/app/src/main/java/com/example/menussemanales/MainActivity.java
@@ -0,0 +1,219 @@
+package com.example.menussemanales;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.example.menussemanales.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private ArrayList<String> listaDatos = new ArrayList<>();
+
+    DialogoCalorias dialogoCalorias;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.opciones_platos,
+                android.R.layout.simple_spinner_item);
+        binding.spinner1.setAdapter(adapter);
+        binding.spinner2.setAdapter(adapter);
+        binding.spinner3.setAdapter(adapter);
+        binding.spinner4.setAdapter(adapter);
+        binding.spinner5.setAdapter(adapter);
+        binding.spinner6.setAdapter(adapter);
+        binding.spinner7.setAdapter(adapter);
+
+
+        //evento del spinner captura la seleccion entre la lista de opciones del Spinner
+        binding.spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicion, long l) {
+//                Toast.makeText(getApplicationContext(), "Seleccionado: " + ingredientesPlato(posicion),
+//                        Toast.LENGTH_SHORT).show();
+                ingredientesPlato(posicion);
+
+//                estado.setText("Seleccion: "+adapterView.getItemAtPosition(posicion).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+//                estado.setText("");
+            }
+        });
+        binding.spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicion, long l) {
+                ingredientesPlato(posicion);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+//                estado.setText("");
+            }
+        });
+        binding.spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicion, long l) {
+                ingredientesPlato(posicion);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+//                estado.setText("");
+            }
+        });
+        binding.spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicion, long l) {
+                ingredientesPlato(posicion);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+//                estado.setText("");
+            }
+        });
+        binding.spinner5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicion, long l) {
+                ingredientesPlato(posicion);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+//                estado.setText("");
+            }
+        });
+        binding.spinner6.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicion, long l) {
+                ingredientesPlato(posicion);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+//                estado.setText("");
+            }
+        });
+        binding.spinner7.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int posicion, long l) {
+                ingredientesPlato(posicion);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+//                estado.setText("");
+            }
+        });
+
+    }
+
+    private String ingredientesPlato(int posicion) {
+        String ingredientes = "";
+        switch (posicion) {
+
+            case 0:
+//                listaDatos.add(ingredientes);
+                break;
+            case 1:
+                ingredientes += "Judías verdes: " +
+                        "Judías verdes ," +
+                        "Puerro";
+                listaDatos.add(ingredientes);
+                break;
+            case 2:
+                ingredientes += "Lentejas con verdura: " +
+                        "Lentejas " +
+                        "Calabacín " +
+                        "Zanahoria " +
+                        "Calabaza ";
+                listaDatos.add(ingredientes);
+                break;
+            case 3:
+                ingredientes +=
+                        "Pollo al ajillo: " +
+                                "Pollo " +
+                                "Ajos ";
+                listaDatos.add(ingredientes);
+                break;
+            case 4:
+                ingredientes += "Salmón plancha: " + "Salmón"
+                ;
+                listaDatos.add(ingredientes);
+                break;
+            case 5:
+                ingredientes += "Crema de calabaza: " +
+                        "Calabaza " +
+                        "Tomate"
+                ;
+                listaDatos.add(ingredientes);
+                break;
+            case 6:
+                ingredientes += "Crema de calabacín: " +
+                        "Calabacín"
+                ;
+                listaDatos.add(ingredientes);
+                break;
+            case 7:
+                ingredientes += "Trucha plancha: " +
+                        "Trucha"
+                ;
+                listaDatos.add(ingredientes);
+                break;
+
+        }
+        return ingredientes;
+    }
+
+    private String calcularCalorias(){
+        return null;
+    }
+    // Menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int seleccion = item.getItemId();
+
+        switch (seleccion) {
+            case R.id.item_ver:
+                Intent intent = new Intent(MainActivity.this, VentanaCompra.class);
+                Bundle miDato = new Bundle();
+//                listaDatos.add("prueba");
+                miDato.putStringArrayList("dato", listaDatos);
+                intent.putExtras(miDato);
+                startActivity(intent);
+                break;
+            case R.id.item_calorias:
+                dialogoCalorias = new DialogoCalorias();
+                dialogoCalorias.show(getSupportFragmentManager(), "dialogo_calo");
+                break;
+        }
+
+        return true;
+    }
+    //fin Menu
+}
