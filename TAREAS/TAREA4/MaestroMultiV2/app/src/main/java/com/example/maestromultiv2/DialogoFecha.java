6a6b8e48065ad84679537c583529ebd6eb0475da
Jose Ace <joseacevp@gmail.com>
2024-02-12 12:28:42 +0100

    Configurar fragmento OK

---
+++ TAREAS/TAREA4/MaestroMultiV2/app/src/main/java/com/example/maestromultiv2/DialogoFecha.java
@@ -0,0 +1,41 @@
+package com.example.maestromultiv2;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class DialogoFecha extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    private OnFechaSeleccionada fechaSeleccionadaListener;
+
+    public interface OnFechaSeleccionada {
+        public void onResultadoFecha(GregorianCalendar fecha);
+    }
+
+    public void setFechaSeleccionadaListener(OnFechaSeleccionada listener) {
+        fechaSeleccionadaListener = listener;
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        GregorianCalendar fecha = new GregorianCalendar(i, i1, i2);
+        fechaSeleccionadaListener.onResultadoFecha(fecha);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int ano = c.get(Calendar.YEAR);
+        int mes = c.get(Calendar.MONTH)+1;
+        int dia = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, ano, mes, dia);
+    }
+
+}
--- TAREAS/TAREA4/MaestroMultiV2/app/src/main/java/com/example/maestromultiv2/ui/ConfiguracionFragment.java
+++ TAREAS/TAREA4/MaestroMultiV2/app/src/main/java/com/example/maestromultiv2/ui/ConfiguracionFragment.java
@@ -1,66 +1,169 @@
 package com.example.maestromultiv2.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.example.maestromultiv2.DialogoFecha;
 import com.example.maestromultiv2.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConfiguracionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ConfiguracionFragment extends Fragment {
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ConfiguracionFragment extends Fragment implements View.OnClickListener, DialogoFecha.OnFechaSeleccionada {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    //variables
+    View view;
+    String heroe, dificultad, fechaSeleccionada, tabla, numero_tabla_selec;
+
+    private String[] heroes = {"Batman",
+            "Hulk",
+            "Iron Man",
+            "Capitan America"};
+
+    private String[] descripcion = {"Matches Malone\u200B El caballero de la noche El caballero oscuro Zurdo Knox\u200B El señor de la noche\u200B",
+            "Hulk, El Increíble Hulk,\u200B El Justiciero, El Hombre Increíble",
+            "Tony Stark, Iron Man (El Hombre de Hierro)",
+            "Nómada, El Capitán, Cap, Steve Rogers, Steve"
+    };
+
+    private int imagenes[] = {R.drawable.batman,
+            R.drawable.huld,
+            R.drawable.iron,
+            R.drawable.capi
+    };
+
 
     public ConfiguracionFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConfiguracionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ConfiguracionFragment newInstance(String param1, String param2) {
         ConfiguracionFragment fragment = new ConfiguracionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracion, container, false);
+        view = inflater.inflate(R.layout.fragment_configuracion, container, false);
+
+        //spinner selector de abatar
+        Spinner selectorAbatar = view.findViewById(R.id.spinner_avatar);
+        AdaptadorPersonalizado a = new AdaptadorPersonalizado(getContext(),
+                R.layout.linea_personaje, heroes);
+        selectorAbatar.setAdapter(a);
+
+        selectorAbatar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //codigo para gestionar lo que ocurre cuando seleccionas una opcion del Spinner
+                heroe = adapterView.getItemAtPosition(i).toString();
+//                Toast.makeText(getContext(), heroe, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        //spinner selector de dificultad
+        Spinner selectorDificultad = view.findViewById(R.id.spinner_dificultad);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.array_dificultad,
+                android.R.layout.simple_spinner_item);
+        selectorDificultad.setAdapter(adapter);
+        selectorDificultad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                dificultad = adapterView.getItemAtPosition(i).toString();
+//                Toast.makeText(getContext(), dificultad, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+  return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    //control de los datos optenidos en la selección de la fecha
+    @Override
+    public void onResultadoFecha(GregorianCalendar fecha) {
+        fechaSeleccionada = fecha.get(Calendar.DAY_OF_MONTH) + "/" + fecha.get(Calendar.MONTH) + "/" + fecha.get(Calendar.YEAR);
+//        Toast.makeText(getContext(), fechaSeleccionada, Toast.LENGTH_SHORT).show();
+    }
+    //adaptador personalizado para el spinner de los heroes
+    public class AdaptadorPersonalizado extends ArrayAdapter {
+
+
+        public AdaptadorPersonalizado(@NonNull Context context,
+                                      int textViewResourceId,
+                                      @NonNull Object[] objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getDropDownView(int position,
+                                    @Nullable View convertView,
+                                    @NonNull ViewGroup parent) {
+            return crearFilaPersonalizada(position, convertView, parent);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position,
+                            @Nullable View convertView,
+                            @NonNull ViewGroup parent) {
+            return crearFilaPersonalizada(position, convertView, parent);
+        }
+
+        private View crearFilaPersonalizada(int position,
+                                            View convertView,
+                                            ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View miFila = inflater.inflate(R.layout.linea_personaje,
+                    parent, false);
+
+            TextView nombre = miFila.findViewById(R.id.nombre);
+            nombre.setText(heroes[position]);
+
+            TextView descipcion = miFila.findViewById(R.id.descripcion);
+            descipcion.setText(descripcion[position]);
+
+            ImageView imagen = miFila.findViewById(R.id.imagen_heroe);
+            imagen.setImageResource(imagenes[position]);
+            return miFila;
+        }
+
+
     }
 }
